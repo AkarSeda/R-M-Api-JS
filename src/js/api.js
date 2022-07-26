7b1feda318ed14f19f9fe87d4de6e2b452600233
seda akar <96084388+AkarSeda@users.noreply.github.com>
2022-07-26 15:44:37 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,1 @@
-/*
-const URL_API = 'https://rickandmortyapi.com/api/character/'
 
-const getData = async (filter) => {
-  const res = filter ? await fetch(`${URL_API}?name=${filter}`) : await fetch(URL_API)
-  const data = await res.json()
-  return data
-}
-
-export default getData
-*/
